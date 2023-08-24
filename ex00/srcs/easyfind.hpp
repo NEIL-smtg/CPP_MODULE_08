0d8db1e19a46415e4f36cbcd3d80762ea559fb65
NEIL-smtg <sunghui2000@hotmail.com>
2023-08-24 23:11:49 +0800

    intial commit

---
+++ ex00/srcs/easyfind.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: suchua < suchua@student.42kl.edu.my>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/24 23:02:43 by suchua            #+#    #+#             */
+/*   Updated: 2023/08/24 23:05:24 by suchua           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+# include <iostream>
+# include <exception>
+
+template<typename T>
+void	easyfind(T type, int find);
+
+# include "easyfind.tpp"
+
+#endif
---
+++ ex00/srcs/easyfind.tpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.tpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: suchua < suchua@student.42kl.edu.my>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/24 23:02:18 by suchua            #+#    #+#             */
+/*   Updated: 2023/08/24 23:06:09 by suchua           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_TPP
+# define EASYFIND_TPP
+
+# include "easyfind.hpp"
+
+template<typename T>
+void	easyfind(T type, int find)
+{
+	
+}
+
+#endif
